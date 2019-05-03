c6654a8733851c46fe8f5c09de4479fe9ea59898
suaaa7 <spuaaa7@gmail.com>
2019-05-03 19:01:02 +0900

    add div to hello

--- hello.go
+++ hello.go
@@ -3,8 +3,20 @@ package main
 import (
 	f "fmt"
 	. "strings"
+	"errors"
 )
+
+func div(i int, j int) (result int, err error) {
+	if j == 0 {
+		err = errors.New("divied by zero")
+		return
+	}
+	result = i / j
+	return
+}
 
 func main() {
 	f.Println(ToUpper("hello world"))
+	result, _ := div(2, 0)
+	f.Println(result)
 }
