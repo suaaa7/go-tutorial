7fe059b4b2442a4eded4b31c8462c4bb26ecae47
suaaa7 <spuaaa7@gmail.com>
2019-05-04 01:31:55 +0900

    add type_switch

---
+++ type_switch.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+func Print(value interface{}) {
+	switch v := value.(type) {
+	case string:
+		fmt.Printf("value is string: %s\n", v)
+	case int:
+		fmt.Printf("value is int: %d\n", v)
+	case Stringer:
+		fmt.Printf("value is Stringer: %s\n", v)
+	}
+}
+
+func main() {
+	Print("abc")
+	Print(100)
+}
